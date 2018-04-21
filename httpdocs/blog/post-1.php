ca78e9cbe2974f634e852aae4995c6239c1d561f
Steven Klein <steven.p.klein@gmail.com>
2018-04-21 18:52:17 -0500

    post nav

--- httpdocs/blog/post-1.php
+++ httpdocs/blog/post-1.php
@@ -5,4 +5,7 @@
     </header>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </article>
+  <nav id="post-navigation" class="post-navigation">
+    <a class="next" href="/blog/post-2.php" title="Navigate to the next post">Next Post</a>
+  </nav>
 <?php include dirname(__FILE__) . "/../../partials/footer.php"; ?>
--- httpdocs/blog/post-3.php
+++ httpdocs/blog/post-3.php
@@ -5,4 +5,7 @@
     </header>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </article>
+  <nav id="post-navigation" class="post-navigation">
+    <a class="previous" href="/blog/post-2.php" title="Navigate to the previous post">Previous Post</a>
+  </nav>
 <?php include dirname(__FILE__) . "/../../partials/footer.php"; ?>
