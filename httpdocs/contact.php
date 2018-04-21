d606bf16e00e3d468b353cebdeaa0f6db636289d
Steven Klein <steven.p.klein@gmail.com>
2018-04-21 15:15:33 -0500

    basic contact form

--- httpdocs/contact.php
+++ httpdocs/contact.php
@@ -3,6 +3,26 @@
     <header>
       <h1>Contact Me</h1>
     </header>
-    <p>If you want to get in touch, hit me up at <a href="mailto:example@example.com" title="email example@example.com">example@example.com</a></p>
+    <p>If you want to get in touch complete the form below.</p>
+    <form action="self" method="post">
+      <fieldset>
+        <legend>Contact Form</legend>
+        <p>
+          <label for="name">Your Name:</label>
+          <input type="text" id="name" name="name" value="" required/>
+        </p>
+        <p>
+          <label for="email">Your Email Address:</label>
+          <input type="email" id="email" name="email" value="" required/>
+        </p>
+        <p>
+          <label for="message">Message:</label>
+          <textarea name="message" id="message" required></textarea>
+        </p>
+        <p>
+          <input type="submit" value="submit" />
+        </p>
+      </fieldset>
+    </form>
   </article>
 <?php include dirname(__FILE__) . "/../templates/footer.php"; ?>
