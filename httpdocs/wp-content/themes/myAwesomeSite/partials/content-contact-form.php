21fa002af34b7acc7e0968ed5f539ddcc6f2e43c
Steven Klein <steven.p.klein@gmail.com>
2018-04-22 03:26:23 -0500

    form processing

--- httpdocs/wp-content/themes/myAwesomeSite/partials/content-contact-form.php
+++ httpdocs/wp-content/themes/myAwesomeSite/partials/content-contact-form.php
@@ -1,34 +1,39 @@
-  <?php $formData = themeProcessForm(); // must be run before headers are sent since it may send headers. ?>
+<?php
+/**
+ * partials content-contact-form.php
+ */
+?>
+
+  <?php $formData = ( isset( $_POST['theme_contact_form_nonce'] ) ) ? themeValidateAndSanitzeFields( themeGetDefaultFormData($_POST) ) : themeGetDefaultFormData();  ?>
 
   <?php if( isset($_GET['sent']) && $_GET['sent'] == 1 ): ?>
     <p class="form-message success">Thank you!  You're message was sent successfully.</p>
-  <?php elseif( isset($formData['errors']['sent']) ): ?>
+  <?php elseif( isset($_GET['error']) && is_string($_GET['error']) ): ?>
     <p class="form-message error"><?php echo $formData['errors']['sent']; ?></p>
   <?php endif; ?>
 
-    <p>If you want to get in touch complete the form below.</p>
-
-    <form action="<?php get_permalink(); ?>" id="contact-form" method="post" novalidate>
+    <form action="<?php echo esc_url( the_permalink() ); ?>" id="contact-form" method="post" novalidate>
       <fieldset>
         <legend>Contact Form</legend>
         <p>* indicates a required field</p>
         <p>
-          <?php themeMayDisplayFieldErrors($formData['errors'], 'name'); ?>
+          <?php themeDisplayFieldErrors($formData['errors'], 'contact-name'); ?>
           <label for="name">Your Name: *</label>
-          <input type="text" id="name" name="name" required value="<?php echo (!empty($formData['fields']['name'])) ? $formData['fields']['name'] : ''; ?>"/>
+          <input type="text" id="name" name="contact-name" required value="<?php echo (!empty($formData['fields']['contact-name'])) ? $formData['fields']['contact-name'] : ''; ?>"/>
         </p>
         <p>
-          <?php themeMayDisplayFieldErrors($formData['errors'], 'email'); ?>
+          <?php themeDisplayFieldErrors($formData['errors'], 'contact-email'); ?>
           <label for="email">Your Email Address: *</label>
-          <input type="email" id="email" name="email" required value="<?php echo (!empty($formData['fields']['email'])) ? $formData['fields']['email'] : ''; ?>"/>
+          <input type="email" id="email" name="contact-email" required value="<?php echo (!empty($formData['fields']['contact-email'])) ? $formData['fields']['contact-email'] : ''; ?>"/>
         </p>
         <p>
-          <?php themeMayDisplayFieldErrors($formData['errors'], 'message'); ?>
+          <?php themeDisplayFieldErrors($formData['errors'], 'contact-message'); ?>
           <label for="message">Message: *</label>
-          <textarea name="message" id="message" required><?php echo (!empty($formData['fields']['message'])) ? $formData['fields']['message'] : ''; ?></textarea>
+          <textarea name="contact-message" id="message" required><?php echo (!empty($formData['fields']['contact-message'])) ? $formData['fields']['contact-message'] : ''; ?></textarea>
         </p>
         <p>
-          <input type="submit" value="submit" />
+          <input type="submit" value="submit"/>
+          <?php wp_nonce_field( "theme_contact_form", "theme_contact_form_nonce", true, true ); ?>
         </p>
       </fieldset>
     </form>
--- httpdocs/wp-content/themes/myAwesomeSite/partials/content.php
+++ httpdocs/wp-content/themes/myAwesomeSite/partials/content.php
@@ -7,9 +7,13 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
   <header>
-		<h1><a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a></h1>
+		<h1><?php the_title(); ?></h1>
 	</header>
 
   <?php the_excerpt(); ?>
+
+  <p>
+    <a href="<?php echo esc_url( get_permalink() ); ?>" title="Read Full Article: <?php echo the_title(); ?>">Read More</a>
+  </p>
 
 </article>
