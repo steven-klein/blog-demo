7a5d4eb1db614245a3998bb546a6cc7c5154571c
Steven Klein <steven.p.klein@gmail.com>
2018-04-21 21:34:29 -0500

    bundle js

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,43 @@
-import buble from 'rollup-plugin-buble';
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
-import uglify from 'rollup-plugin-uglify';
-import replace from 'rollup-plugin-replace';
-
-let cache;
+import buble from 'rollup-plugin-buble'
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+import uglify from 'rollup-plugin-uglify'
+import replace from 'rollup-plugin-replace'
+import includePaths from 'rollup-plugin-includepaths'
 
 export default {
-    entry: 'resources/assets/js/app.js',
-    dest: 'httpdocs/assets/js/app.js',
-    cache: cache,
-    format: 'iife',
-    useStrict: true,
-    plugins: [
-        resolve({
-            jsnext: true, // Default: false
-            main: true, // Default: true
-            browser: true,
-            extensions: ['.js', '.json']
-        }),
-        commonjs({
-            include: ['node_modules/**', 'resources/**'],
-            extensions: ['.js', '.json'],
-            // if false then skip sourceMap generation for CommonJS modules
-            sourceMap: true, // Default: true
-        }),
-        buble(),
-        replace({
-            exclude: 'node_modules/**',
-            ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
-        }),
-        (process.env.NODE_ENV === 'production' && uglify())
-    ],
-    sourceMap: (process.env.NODE_ENV !== 'production') ? true : false
+  entry: 'resources/assets/js/app.js',
+  dest: 'httpdocs/assets/js/app.js',
+  format: 'iife',
+  useStrict: true,
+  globals: {
+    jquery: '$'
+  },
+  plugins: [
+    includePaths({
+      include: {},
+      paths: ['resources/assets/js'],
+      external: [],
+      extensions: ['.js', '.json']
+    }),
+    resolve({
+      jsnext: true, // Default: false
+      main: true, // Default: true
+      browser: true,
+      extensions: ['.js', '.json']
+    }),
+    commonjs({
+      include: ['node_modules/**', 'resources/**'],
+      extensions: ['.js', '.json'],
+      // if false then skip sourceMap generation for CommonJS modules
+      sourceMap: true, // Default: true
+    }),
+    buble(),
+    replace({
+      exclude: 'node_modules/**',
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || 'development'),
+    }),
+    (process.env.NODE_ENV === 'production' && uglify())
+  ],
+  sourceMap: (process.env.NODE_ENV !== 'production') ? true : false
 }
