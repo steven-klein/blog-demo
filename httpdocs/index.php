eb83bf7ab5572daba44d24a69307171000e45f18
Steven Klein <steven.p.klein@gmail.com>
2018-04-22 00:37:56 -0500

    wp conversion

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -1,20 +1,17 @@
-<?php include dirname(__FILE__) . "/../partials/header.php"; ?>
-<article>
-  <header>
-    <h1>Welcome to my awesome website</h1>
-  </header>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <h2>Check out my recent blog entries</h2>
-  <ul>
-    <li>
-      <a href="/blog/post-1.php" title="Blog post entry number 1">Blog post entry number 1</a>
-    </li>
-    <li>
-      <a href="/blog/post-2.php" title="Blog post entry number 2">Blog post entry number 2</a>
-    </li>
-    <li>
-      <a href="/blog/post-3.php" title="Blog post entry number 3">Blog post entry number 3</a>
-    </li>
-  </ul>
-</article>
-<?php include dirname(__FILE__) . "/../partials/footer.php"; ?>
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
+
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
