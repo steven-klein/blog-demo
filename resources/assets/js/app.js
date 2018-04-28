1cdf4729a7c86545cb3178980a2c6832012f9646
Steven Klein <steven.p.klein@gmail.com>
2018-04-27 22:53:58 -0500

    resources

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,32 +2,5 @@
  * assets/js/app.js
  */
 
- (function ($, window, document) {
-
-  // validate our contact form.
-  $("document").ready(setupContactFormValidation);
-
-  /**
-   * attach the jquery.validate form handler to our contact form.
-   *
-   * @return void
-   */
-  function setupContactFormValidation () {
-    $("#contact-form").validate({
-      errorPlacement: function (error, element) {
-        error.insertBefore(element);
-      },
-      messages: {
-        name: "Your name is a required field.",
-        email: {
-          required: "Your email address is a required field.",
-          email: function() {
-            return $("#email").val() + " appears to be an invalid email address."
-          }
-        },
-        message: "Please let me know what you're getting in contact about."
-      }
-    });
-  }
-
-}(window.jQuery, window, document) );
+// App Modules
+import "./contactForm";
